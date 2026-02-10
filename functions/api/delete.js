f8631ff1e7d84b84136cae8a51e82a848516f959
NoteFinder <165768662+NoteFinder@users.noreply.github.com>
2026-02-10 20:24:28 +0800

    Create delete.js

---
+++ functions/api/delete.js
@@ -0,0 +1,13 @@
+export async function onRequestPost(context) {
+  const { env, request } = context;
+  const { id } = await request.json();
+
+  try {
+    await env.DB.prepare("DELETE FROM themes WHERE id = ?").bind(id).run();
+    return new Response(JSON.stringify({ success: true }), {
+      headers: { "Content-Type": "application/json" }
+    });
+  } catch (err) {
+    return new Response(err.message, { status: 500 });
+  }
+}
