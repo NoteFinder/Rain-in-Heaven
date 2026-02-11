1654ddf6c68b045cdd46a5d02f3c6bfe71e510ed
NoteFinder <165768662+NoteFinder@users.noreply.github.com>
2026-02-11 16:49:45 +0800

    Update add.js

--- functions/api/add.js
+++ functions/api/add.js
@@ -1,11 +1,11 @@
 export async function onRequestPost(context) {
   const { env, request } = context;
-  const { name, description, image, link } = await request.json();
+  const { name, description, image, link, try_link } = await request.json();
 
   try {
     await env.DB.prepare(
-      "INSERT INTO themes (name, description, image, link) VALUES (?, ?, ?, ?)"
-    ).bind(name, description, image, link).run();
+      "INSERT INTO themes (name, description, image, link, try_link) VALUES (?, ?, ?, ?, ?)"
+    ).bind(name, description, image, link, try_link).run();
 
     return new Response(JSON.stringify({ success: true }), {
       headers: { "Content-Type": "application/json" }
